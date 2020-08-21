1a161519ab4a3ec9c4e9d104d6ba2c861d8516f4
SuttidaCh <suttida.20992@gmail.com>
2020-08-21 16:53:39 +0700

    enter

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     <p>
       <br />
     </p>
-    <HelloWorld msg="LOVE LIKE YOU" />
+    <HelloWorld msg="LOVE LIKE DYNAMITE" />
     <br />
     <div class="row">
       <div class="col-4">
@@ -37,6 +37,45 @@
       <div class="col-4">
         <img
           src="https://pbs.twimg.com/profile_images/1293181369665744897/hFU3cZPa_400x400.jpg"
+          class="rounded-circle"
+          width="50%"
+          
+        />
+        <p>
+          <br />
+        </p>
+        <Button />
+      </div>
+    </div>
+   <p>
+      <br />
+    </p>
+     <div class="row">
+      <div class="col-4">
+        <img
+          src="https://pbs.twimg.com/profile_images/1292848228535164930/czxwXw2s_400x400.jpg"
+          class="rounded-circle"
+          width="50%"
+        />
+        <p>
+          <br />
+        </p>
+        <Button />
+      </div>
+      <div class="col-4">
+        <img
+          src="https://pbs.twimg.com/profile_images/1292865033123147777/oJFkqbOi_400x400.jpg"
+          class="rounded-circle"
+          width="50%"
+        />
+        <p>
+          <br />
+        </p>
+        <Button />
+      </div>
+      <div class="col-4">
+        <img
+          src="https://pbs.twimg.com/profile_images/1292850607003336704/OG2jBS6c_400x400.jpg"
           class="rounded-circle"
           width="50%"
           
