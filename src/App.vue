72d7b4e95673941b43cac40e5a8282e68289f26b
SuttidaCh <suttida.20992@gmail.com>
2020-08-25 23:31:26 +0700

    enter

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     
     <img
-      src="https://pbs.twimg.com/profile_images/1294650535702753282/n0jc9pBg_400x400.jpg"
+      src="img\p.jpg"
       class="rounded-circle"
       width="20%"
     />
@@ -14,7 +14,7 @@
     <div class="row">
       <div class="col-4">
         <img
-          src="https://pbs.twimg.com/profile_images/1292843792710676482/NebuzRYw_400x400.jpg"
+          src="img/v.jpg"
           class="rounded-circle"
           width="50%"
         />
@@ -25,7 +25,7 @@
       </div>
       <div class="col-4">
         <img
-          src="https://pbs.twimg.com/profile_images/1292844473622421505/HUFG4PeZ_400x400.jpg"
+          src="img/jh.jpg"
           class="rounded-circle"
           width="50%"
         />
@@ -36,7 +36,7 @@
       </div>
       <div class="col-4">
         <img
-          src="https://pbs.twimg.com/profile_images/1293181369665744897/hFU3cZPa_400x400.jpg"
+          src="img/rm.jpg"
           class="rounded-circle"
           width="50%"
           
@@ -53,7 +53,7 @@
      <div class="row">
       <div class="col-4">
         <img
-          src="https://pbs.twimg.com/profile_images/1292848228535164930/czxwXw2s_400x400.jpg"
+          src="img/yg.jpg"
           class="rounded-circle"
           width="50%"
         />
@@ -64,7 +64,7 @@
       </div>
       <div class="col-4">
         <img
-          src="https://pbs.twimg.com/profile_images/1292865033123147777/oJFkqbOi_400x400.jpg"
+          src="img/j.jpg"
           class="rounded-circle"
           width="50%"
         />
@@ -75,7 +75,7 @@
       </div>
       <div class="col-4">
         <img
-          src="https://pbs.twimg.com/profile_images/1292850607003336704/OG2jBS6c_400x400.jpg"
+          src="img/jk.jpg"
           class="rounded-circle"
           width="50%"
           
